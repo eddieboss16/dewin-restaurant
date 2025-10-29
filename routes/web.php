d34c5938fc8fceda7b50197831e2ba19656cfcdf
edu <edwinnjoroge001@gmail.com>
2025-10-29 15:27:16 +0300

    admin dashboard, route and controller created successfully

--- routes/web.php
+++ routes/web.php
@@ -2,9 +2,13 @@
 
 use Illuminate\Support\Facades\Route;
 
+use App\Http\Controllers\HomeController;
+
 Route::get('/', function () {
     return view('welcome');
 });
+
+route::get('/home', [HomeController::class,'index']);
 
 Route::middleware([
     'auth:sanctum',
